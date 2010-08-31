3b771ef8fa5971663ecc20652cf5af1211d1f539
Clayten <clayten@kelora.org>
2010-08-31 16:29:12 -0700

    reloading from the menu

--- quiz31/maze.rb
+++ quiz31/maze.rb
@@ -343,6 +343,7 @@ class Maze
         when /^d(elay)?(=|\s?)([0-9.]+)/ ; options[:delay] = $3.to_f ; result = "Delay is #{options[:delay]}"
         when /^([ijkl])/ ; maze.move dirs = {'i' => :north, 'j' => :west, 'k' => :south, 'l' => :east}[$1] if maze.highlighted_cell
         when /^f/ ; choices = maze.highlighted_cell.not_walked_on_neighbors ; next unless choices.length == 1 ; maze.move choices.first.first
+        when /^r/ ; load __FILE__
         when /^u/
             ml = maze.move_log.dup
             move = ml.pop

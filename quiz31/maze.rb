6158af6f447c3f81dc47ddfb7a486f74b21b563d
Clayten <clayten@kelora.org>
2010-09-03 19:48:13 -0700

    wiping @mapped as well

--- quiz31/maze.rb
+++ quiz31/maze.rb
@@ -192,7 +192,7 @@ class Maze
   end
 
   def wipe_designations
-    @highlighted_cell = @start_cell = @end_cell = @generated = @solved = @move_log = nil
+    @highlighted_cell = @start_cell = @end_cell = @generated = @solved = @mapped = @move_log = nil
   end
 
   def setup_board options = {}
